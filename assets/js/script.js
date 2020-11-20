0e5a7394f9aca0ed89c8fd49e0da531154772906
corycalaway <cory.c.calaway@gmail.com>
2020-11-19 18:54:13 -0700

    reloads saved data to wrong first section only

--- assets/js/script.js
+++ assets/js/script.js
@@ -273,6 +273,23 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+  var savedTasks = localStorage.getItem("tasks");
+
+  if (!savedTasks) {
+    return false;
+  }
+
+  savedTasks = JSON.parse(savedTasks);
+
+
+  // loop through savedTasks array
+for (var i = 0; i < savedTasks.length; i++) {
+  // pass each task object into the `createTaskEl()` function
+  createTaskEl(savedTasks[i]);
+}
+}
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
@@ -285,6 +302,7 @@ pageContentEl.addEventListener("drop", dropTaskHandler);
 
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
+loadTasks();
 //document.getElementById()
 
 //document.createElement()
@@ -293,4 +311,3 @@ pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
 //event.stopPropagation()
 
-//4.4.8
